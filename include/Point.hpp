10eb860fa80e82dbeab7b4a7512b4269b8abfda8
spartanPAGE <spartanpageagain@gmail.com>
2014-06-29 12:29:11 +0200

    Added definitions and declarations for set of static methods   -zero()   -one()   -unitX()   -unitY()

--- include/Point.hpp
+++ include/Point.hpp
@@ -5,5 +5,26 @@ namespace vulture{
     struct Point{
         int32_t x, y;
         Point(int32_t = 0, int32_t = 0);
+
+        inline static Point zero();
+        inline static Point one();
+        inline static Point unitX();
+        inline static Point unitY();
     };
+
+    Point Point::zero(){
+        return Point(0, 0);
+    }
+
+    Point Point::one(){
+        return Point(1, 1);
+    }
+
+    Point Point::unitX(){
+        return Point(1, 0);
+    }
+
+    Point Point::unitY(){
+        return Point(0, 1);
+    }
 }

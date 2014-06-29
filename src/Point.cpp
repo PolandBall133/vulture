1b96278cb125e7ef54dee3facc634fe8b2149f31
spartanPAGE <spartanpageagain@gmail.com>
2014-06-29 12:36:04 +0200

    Added arithmetical methods definitions for Point

--- src/Point.cpp
+++ src/Point.cpp
@@ -4,4 +4,64 @@ namespace vulture{
     Point::Point(int32_t x, int32_t y):
         x(x),
         y(y){}
+
+    bool
+    Point::operator==(const Point &other) const{
+        return x == other.x && y == other.y;
+    }
+
+    bool
+    Point::operator!=(const Point &other) const{
+        return !operator==(other);
+    }
+
+    Point
+    Point::operator+(const Point &other) const{
+        return Point(x+other.x, y+other.y);
+    }
+
+    Point
+    Point::operator-(const Point &other) const{
+        return Point(x-other.x, y-other.y);
+    }
+
+    Point
+    Point::operator*(const Point &other) const{
+        return Point(x*other.x, y*other.y);
+    }
+
+    Point
+    Point::operator/(const Point &other) const{
+        return Point(x/other.x, y/other.y);
+    }
+
+    Point
+    Point::operator%(const Point &other) const{
+        return Point(x%other.x, y%other.y);
+    }
+
+    Point &
+    Point::operator+=(const Point &other){
+        return operator=(operator+(other));
+    }
+
+    Point &
+    Point::operator-=(const Point &other){
+        return operator=(operator-(other));
+    }
+
+    Point &
+    Point::operator*=(const Point &other){
+        return operator=(operator*(other));
+    }
+
+    Point &
+    Point::operator/=(const Point &other){
+        return operator=(operator/(other));
+    }
+
+    Point &
+    Point::operator%=(const Point &other){
+        return operator=(operator%(other));
+    }
 }

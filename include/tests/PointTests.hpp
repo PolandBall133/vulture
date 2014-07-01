b38713ef025f46547e08735f7d63350754a76466
spartanPAGE <spartanpageagain@gmail.com>
2014-07-01 19:39:15 +0200

    Changed access specifier for PointFixture from default (private) to public; Changed invalid fixture name in suite; from *Test to *Fixture

--- include/tests/PointTests.hpp
+++ include/tests/PointTests.hpp
@@ -10,8 +10,8 @@
 
 namespace vulture{
     namespace test{
-        class PointFixture : CppUnit::TestFixture{
-            CPPUNIT_TEST_SUITE(PointTest);
+        class PointFixture : public CppUnit::TestFixture{
+            CPPUNIT_TEST_SUITE(PointFixture);
                 CPPUNIT_TEST(arithmetical_test);
             CPPUNIT_TEST_SUITE_END();
         private:

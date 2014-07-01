457851698e7c6e21dc5377628f078d2415bdb15c
spartanPAGE <spartanpageagain@gmail.com>
2014-07-01 19:27:56 +0200

    Added CppUnit headers; Added tested class `Point` header

--- include/tests/PointTests.hpp
+++ include/tests/PointTests.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCase.h>
+
+#include "Point.hpp"
+
 namespace vulture{
     namespace test{
     }

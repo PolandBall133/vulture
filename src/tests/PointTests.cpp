a40d087f69276300df0431ecfd7c71ef9229fe69
spartanPAGE <spartanpageagain@gmail.com>
2014-07-01 19:48:17 +0200

    Added arithmetical_test definition; Added bunch of asserts for arithmetical tests

--- src/tests/PointTests.cpp
+++ src/tests/PointTests.cpp
@@ -3,5 +3,24 @@
 namespace vulture{
     namespace test{
         CPPUNIT_TEST_SUITE_REGISTRATION(PointFixture);
+
+        void
+        PointFixture::arithmetical_test(){
+            // "+"
+            a = Point(20, 20) + Point(30, 30);
+            CPPUNIT_ASSERT(a == Point(50, 50));
+            // "-"
+            a -= Point(15, 15);
+            CPPUNIT_ASSERT(a == Point(35, 35));
+            // "*"
+            a *= Point(1, 2);
+            CPPUNIT_ASSERT(a == Point(35, 70));
+            // "/"
+            a = Point(12, 9) / Point(4, 3);
+            CPPUNIT_ASSERT(a == Point(3, 3));
+            // "%"
+            a = Point(15, 3) % Point(2, 3);
+            CPPUNIT_ASSERT(a == Point(15%2, 3%3));
+        }
     }
 }

bb8c10289a3fa9c69cfab8f25d91493c5eb9ea3d
spartanPAGE <spartanpageagain@gmail.com>
2014-07-03 10:32:20 +0200

    Added RectangleTests header and typical-for-header lines

---
+++ include/tests/RectangleTest.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCase.h>
+
+#include "Point.hpp"
+
+namespace vulture{
+    namespace test{
+
+    }
+}

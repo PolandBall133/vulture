fdaf592037bb825636931e63dd2425e501bb010a
spartanPAGE <spartanpageagain@gmail.com>
2014-07-03 10:27:28 +0200

    Added bunch of useful methods :P

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -7,5 +7,11 @@ namespace vulture{
     struct Rectangle{
         int32_t x, y, width, height;
         Rectangle(int32_t = 0, int32_t = 0, int32_t = 0, int32_t = 0);
+
+        bool isEmpty() const;
+        bool equals(const Rectangle &) const;
+        bool hasIntersection(const Rectangle &) const;
+        Rectangle getIntersect(const Rectangle &) const;
+        Rectangle getUnion(const Rectangle &) const;
     };
 }

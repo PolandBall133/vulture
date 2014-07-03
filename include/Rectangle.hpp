95d9d217df1608203be3a4cadc14c1a496eb5fe5
spartanPAGE <spartanpageagain@gmail.com>
2014-07-03 10:26:25 +0200

    Added Rectangle; Added fields for rectangle; Added Rectangle constructor with default values

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -1,5 +1,11 @@
 #pragma once
+
 #include <cinttypes>
 
 namespace vulture{
+
+    struct Rectangle{
+        int32_t x, y, width, height;
+        Rectangle(int32_t = 0, int32_t = 0, int32_t = 0, int32_t = 0);
+    };
 }

c817a1a3bdffe1a0b92061c95de7f75bc5392740
spartanPAGE <spartanpageagain@gmail.com>
2014-07-07 10:47:24 +0200

    Added Vector2 constructors definition

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,5 +1,11 @@
 #include "Vector2.hpp"
 
 namespace vulture{
+    constexpr Vector2::Vector2(double x, double y)
+        : x(x),
+          y(y){}
 
+    constexpr Vector2::Vector2(double scalar)
+        : x(scalar),
+          y(scalar){}
 }

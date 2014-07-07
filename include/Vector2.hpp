7da4d4b2282eaf9342bc77327884316f252b2f38
spartanPAGE <spartanpageagain@gmail.com>
2014-07-07 10:06:39 +0200

    Added constexpr qualifier for constructors; Added bunch of useful `inline static constexpr` methods :P

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -5,7 +5,23 @@ namespace vulture{
     struct Vector2{
         double x, y;
 
-        Vector2(double = 0.0, double = 0.0);
-        Vector2(double);
+        constexpr Vector2(double = 0.0, double = 0.0);
+        constexpr Vector2(double);
+
+        inline static constexpr Vector2 zero(){
+            return Vector2(0.0, 0.0);
+        }
+
+        inline static constexpr Vector2 one(){
+            return Vector2(1.0, 1.0);
+        }
+
+        inline static constexpr Vector2 unitX(){
+            return Vector2(1.0, 0.0);
+        }
+
+        inline static constexpr Vector2 unitY(){
+            return Vector2(0.0, 1.0);
+        }
     };
 }

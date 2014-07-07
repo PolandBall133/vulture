e688d854ff3fe03394d52f6ac2ba90bfca7fc30b
spartanPAGE <spartanpageagain@gmail.com>
2014-07-07 11:19:21 +0200

    Added some more operators definition; Added bunch of headers

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,4 +1,6 @@
 #include "Vector2.hpp"
+#include <cmath>
+#include <limits>
 
 namespace vulture{
     Vector2::Vector2(double x, double y)
@@ -47,5 +49,21 @@ namespace vulture{
     Vector2 &
     Vector2::operator/=(const Vector2 &other){
         return operator=(operator/(other));
+    }
+
+        Vector2
+    Vector2::operator-() const{
+        return Vector2(-x, -y);
+    }
+
+    bool
+    Vector2::operator==(const Vector2 &other) const{
+        constexpr double epsilon = std::numeric_limits<double>::epsilon();
+        return (std::abs(x-other.x)<epsilon && std::abs(y-other.y)<epsilon);
+    }
+
+    bool
+    Vector2::operator!=(const Vector2 &other) const{
+        return !operator==(other);
     }
 }

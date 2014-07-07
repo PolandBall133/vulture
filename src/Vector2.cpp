7d33dc8bbad3b7fc16de77da5adf46c07ab69c28
spartanPAGE <spartanpageagain@gmail.com>
2014-07-07 11:12:25 +0200

    Deleted constexpr from constructors; Added operators definition

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,11 +1,51 @@
 #include "Vector2.hpp"
 
 namespace vulture{
-    constexpr Vector2::Vector2(double x, double y)
+    Vector2::Vector2(double x, double y)
         : x(x),
           y(y){}
 
-    constexpr Vector2::Vector2(double scalar)
+    Vector2::Vector2(double scalar)
         : x(scalar),
           y(scalar){}
+
+    Vector2
+    Vector2::operator+(const Vector2 &other) const{
+        return Vector2(x+other.x, y+other.y);
+    }
+
+    Vector2
+    Vector2::operator-(const Vector2 &other) const{
+        return Vector2(x-other.x, y-other.y);
+    }
+
+    Vector2
+    Vector2::operator*(const Vector2 &other) const{
+        return Vector2(x*other.x, y*other.y);
+    }
+
+    Vector2
+    Vector2::operator/(const Vector2 &other) const{
+        return Vector2(x/other.x, y/other.y);
+    }
+
+    Vector2 &
+    Vector2::operator+=(const Vector2 &other){
+        return operator=(operator+(other));
+    }
+
+    Vector2 &
+    Vector2::operator-=(const Vector2 &other){
+        return operator=(operator-(other));
+    }
+
+    Vector2 &
+    Vector2::operator*=(const Vector2 &other){
+        return operator=(operator*(other));
+    }
+
+    Vector2 &
+    Vector2::operator/=(const Vector2 &other){
+        return operator=(operator/(other));
+    }
 }

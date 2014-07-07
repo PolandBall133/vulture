8d434326c2324504580e3f5899bf3951490189fa
spartanPAGE <spartanpageagain@gmail.com>
2014-07-07 11:31:06 +0200

    Registered arithmetical_test in CPPUNIT_TEST suite

--- include/tests/Vector2Tests.hpp
+++ include/tests/Vector2Tests.hpp
@@ -12,6 +12,7 @@ namespace vulture{
     namespace test{
         class Vector2Fixture : public CppUnit::TestFixture{
         CPPUNIT_TEST_SUITE(Vector2Fixture);
+            CPPUNIT_TEST(arithmetical_test);
         CPPUNIT_TEST_SUITE_END();
         private:
             Vector2 a, b;

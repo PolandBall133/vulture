41d6dcfb487d82998243b99df1fa39a9aa429c08
spartanPAGE <spartanpageagain@gmail.com>
2014-07-07 11:28:48 +0200

    Added prepared Vector2Tests with Vector2Fixture.
    And some lines. Typical lines.

---
+++ include/tests/Vector2Tests.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCase.h>
+
+#include "Vector2.hpp"
+
+namespace vulture{
+    namespace test{
+        class Vector2Fixture : public CppUnit::TestFixture{
+        CPPUNIT_TEST_SUITE(Vector2Fixture);
+        CPPUNIT_TEST_SUITE_END();
+        private:
+        public:
+        };
+    }
+}
+

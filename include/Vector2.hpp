967851e3041ae14b9857b7a361d2f9131b8eb187
spartanPAGE <spartanpageagain@gmail.com>
2014-07-07 10:11:19 +0200

    Added bunch of static methods from MonoGame::Vector2

--- include/Vector2.hpp
+++ include/Vector2.hpp
@@ -27,6 +27,22 @@ namespace vulture{
         //TODO: normalized()
         void normalize();
 
+        /** static methods from MonoGame,
+          * unit-tests are not needed.
+          */
+        static Vector2 barycentric(const Vector2 &, const Vector2 &, const Vector2 &, double, double);
+        static Vector2 catmullRom(const Vector2 &, const Vector2 &, const Vector2 &, const Vector2 &, double);
+        static Vector2 clamp(const Vector2 &, const Vector2 &, const Vector2 &);
+        static double distance(const Vector2 &, const Vector2 &);
+        static double distanceSquared(const Vector2 &, const Vector2 &);
+        static double dot(const Vector2 &, const Vector2 &);
+        static Vector2 reflect(const Vector2 &, const Vector2 &);
+        static Vector2 hermite(const Vector2 &, const Vector2 &, const Vector2 &, const Vector2 &, double);
+        static Vector2 lerp(const Vector2 &, const Vector2 &, double);
+        static Vector2 max(const Vector2 &, const Vector2 &);
+        static Vector2 min(const Vector2 &, const Vector2 &);
+        static Vector2 smoothStep(const Vector2 &, const Vector2 &, double);
+
         inline static constexpr Vector2 zero(){
             return Vector2(0.0, 0.0);
         }

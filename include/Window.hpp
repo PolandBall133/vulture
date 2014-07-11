ede593372017e9bebcc999c813b7cb469238445a
spartanPAGE <spartanpageagain@gmail.com>
2014-07-11 09:20:22 +0200

    Added `enum with flags` for `Window class`

--- include/Window.hpp
+++ include/Window.hpp
@@ -4,5 +4,19 @@
 namespace vulture{
     class Window{
     public:
+        enum Flags : uint32_t{
+            FullScreen = 1,
+            Shown = 4,
+            Hidden = 8,
+            BorderLess = 16,
+            Resizable = 32,
+            Minimized = 64,
+            Maximized = 128,
+            Input_Grabbed = 256,
+            Input_Focus = 512,
+            Mouse_Focus = 1024,
+            Foreign = 2048,
+            FullScreen_Desktop = 4096 | FullScreen
+        };
     };
 }

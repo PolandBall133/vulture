962e886a8be1bee9bf4bc2909ee4d421f1405361
spartanPAGE <spartanpageagain@gmail.com>
2014-07-11 09:22:15 +0200

    Added bunch of virtual accessors

--- include/Window.hpp
+++ include/Window.hpp
@@ -18,5 +18,12 @@ namespace vulture{
             Foreign = 2048,
             FullScreen_Desktop = 4096 | FullScreen
         };
+
+        virtual const std::string &title() const = 0;
+        virtual void title(const std::string &) = 0;
+        virtual size_t width() const = 0;
+        virtual void width(size_t) = 0;
+        virtual size_t height() const = 0;
+        virtual void height(size_t) = 0;
     };
 }

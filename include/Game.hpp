f213177a0d563d014de9bdd43de829669b4707f9
spartanPAGE <spartanpageagain@gmail.com>
2014-07-15 00:26:22 +0200

    Added pure virtual `onInitialize` method to `Game`; Added pure virtual `onFinalize` method to `Game`; Added pure virtual `onDraw` method to `Game`;

--- include/Game.hpp
+++ include/Game.hpp
@@ -5,5 +5,9 @@ namespace vulture{
     class Game{
     public:
         void start();
+    protected:
+        virtual void onInitialize() = 0;
+        virtual void onFinalize() = 0;
+        virtual void onDraw() = 0;
     };
 }

0eb0e35becfac0166ffc9a5c73911397ce349e3e
spartanPAGE <spartanpageagain@gmail.com>
2014-07-15 00:28:37 +0200

    Added some `TODO`'s

--- include/Game.hpp
+++ include/Game.hpp
@@ -6,10 +6,12 @@ namespace vulture{
     public:
         void start();
     protected:
+        //TODO: onUpdate(ticks)
         virtual void onInitialize() = 0;
         virtual void onFinalize() = 0;
         virtual void onDraw() = 0;
 
+        //TODO: update(ticks)
         void initialize();
         void finalize();
 

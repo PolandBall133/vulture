aeb7ff4d90b42e93b564bc1131b2206a066f7b97
spartanPAGE <spartanpageagain@gmail.com>
2014-07-16 01:18:53 +0200

    Well. These are definitions... 01:18::16:07:2014

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,39 @@
-#include "Game.cpp"
+#include "Game.hpp"
 
 namespace vulture{
+    void
+    Game::start(){
+        initialize();
+        auto new_time = std::chrono::system_clock::now();
+        auto old_time = new_time;
+        std::chrono::duration<double> elapsed_time;
+        while(!quit){
+            new_time = std::chrono::system_clock::now();
+            elapsed_time = new_time - old_time;
+            old_time = new_time;
+            update(elapsed_time);
+            draw();
+        }
+        finalize();
+    }
+
+    void
+    Game::update(std::chrono::duration<double> elapsed_time){
+        onUpdate(elapsed_time);
+    }
+
+    void
+    Game::initialize(){
+        onInitialize();
+    }
+
+    void
+    Game::finalize(){
+        onFinalize();
+    }
+
+    void
+    Game::draw(){
+        onDraw();
+    }
 }

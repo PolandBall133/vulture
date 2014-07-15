d709d3c3a4629e1b38fc83425062fa209ba64892
spartanPAGE <spartanpageagain@gmail.com>
2014-07-16 01:18:02 +0200

    Added "TODO"; `update`, `initialize`, `finalize` and the new `draw` methods of `Game` are now private

--- include/Game.hpp
+++ include/Game.hpp
@@ -3,6 +3,7 @@
 #include <chrono>
 
 namespace vulture{
+    //TODO: Tests. Mocks? Something? i don't know
     class Game{
     public:
         void start();
@@ -12,10 +13,11 @@ namespace vulture{
         virtual void onFinalize() = 0;
         virtual void onDraw() = 0;
 
+        bool quit;
+    private:
         void update(std::chrono::duration<double>);
         void initialize();
         void finalize();
-
-        bool quit;
+        void draw();
     };
 }

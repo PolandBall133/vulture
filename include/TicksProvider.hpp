4f64d0a30fa78616bdb5ba05db190f6b6d084efb
spartanPAGE <spartanpageagain@gmail.com>
2014-07-16 20:08:25 +0200

    changed return type of `TicksProvider::ticks` from double to time_t

--- include/TicksProvider.hpp
+++ include/TicksProvider.hpp
@@ -1,9 +1,10 @@
 #pragma once
+#include <ctime>
 
 namespace vulture{
     //TODO: Tests. Mocks? Something? i don't know
     class TicksProvider{
     public:
-        virtual double ticks() = 0;
+        virtual time_t ticks() = 0;
     };
 }

8cd902288f7a05f0d709a39577a6ddfa7a3b35b4
spartanPAGE <spartanpageagain@gmail.com>
2014-07-16 10:35:10 +0200

    Added `Window` constructor definition

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,4 +1,9 @@
 #include "Window.hpp"
 
 namespace vulture{
+    Window::Window(const std::string &title, const Rectangle &rect, const Flags flags):
+        _title(title),
+        _rect(rect),
+        _flags(flags)
+    {}
 }

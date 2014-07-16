9b3a256f73d19dc5d603641499506bdc8cde9a61
spartanPAGE <spartanpageagain@gmail.com>
2014-07-16 23:42:03 +0200

    Update Color.hpp
    shh, bracket

--- include/Color.hpp
+++ include/Color.hpp
@@ -13,6 +13,7 @@ namespace vulture{
 
         static inline const Color Transparent(){
             return Color(0x00, 0x00, 0x00, 0x00);
+        }
 
         static inline const Color AliceBlue(){
             return Color(0xf0, 0xf8, 0xff, 0xff);

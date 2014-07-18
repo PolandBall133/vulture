9d834fd0face08292f6d18745b323342529a57fa
spartanPAGE <spartanpageagain@gmail.com>
2014-07-18 14:17:14 +0200

    Renamed `Renderer::"drawColor"` to `drawingColor`

--- include/graphics/Renderer.hpp
+++ include/graphics/Renderer.hpp
@@ -7,6 +7,6 @@ namespace vulture{
     public:
         virtual void clear() = 0;
         virtual void present() = 0;
-        virtual void drawColor(const Color &) = 0;
+        virtual void drawingColor(const Color &) = 0;
     };
 }

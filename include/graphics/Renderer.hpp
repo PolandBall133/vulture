3c457bdbf1f7282eeb23464240eda88a50c4e67a
spartanPAGE <spartanpageagain@gmail.com>
2014-07-18 14:11:49 +0200

    Added public virtual abstract `Renderer::{clear, present, drawColor}` methods

--- include/graphics/Renderer.hpp
+++ include/graphics/Renderer.hpp
@@ -1,6 +1,12 @@
 #pragma once
 
+#include "Color.hpp"
+
 namespace vulture{
     class Renderer{
+    public:
+        virtual void clear() = 0;
+        virtual void present() = 0;
+        virtual void drawColor(const Color &) = 0;
     };
 }

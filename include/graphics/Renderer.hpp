9ed004805080108d535b33c9611c867d8502f581
spartanPAGE <spartanpageagain@gmail.com>
2014-07-18 14:29:17 +0200

    `Renderer` now requiers `weak pointer to Window` in constructor

--- include/graphics/Renderer.hpp
+++ include/graphics/Renderer.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "Color.hpp"
+#include "Window.hpp"
 
 namespace vulture{
     class Renderer{
     public:
+        Renderer(std::weak_ptr<Window>);
         virtual void clear() = 0;
         virtual void present() = 0;
         virtual void drawingColor(const Color &) = 0;

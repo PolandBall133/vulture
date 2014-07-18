2d6d81c6d27951445407d8d6e859a9d50085a2bb
spartanPAGE <spartanpageagain@gmail.com>
2014-07-19 01:48:00 +0200

    Added simply `Size` struct (with empty constructor)

--- include/Size.hpp
+++ include/Size.hpp
@@ -2,4 +2,12 @@
 #include <cinttypes>
 
 namespace vulture{
+    struct Size{
+        Size(uint32_t w, uint32_t h):
+            width(w),
+            height(h){}
+
+        uint32_t width;
+        uint32_t height;
+    };
 }

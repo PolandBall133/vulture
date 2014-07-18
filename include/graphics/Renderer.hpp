efbc8aa41a9f41baef36994f7df3648ee5205a8f
spartanPAGE <spartanpageagain@gmail.com>
2014-07-19 01:43:10 +0200

    Added abstract accessors `Renderer::{viewPort, scale}`

--- include/graphics/Renderer.hpp
+++ include/graphics/Renderer.hpp
@@ -2,6 +2,8 @@
 
 #include "Color.hpp"
 #include "Window.hpp"
+#include "Rectangle.hpp"
+#include "Vector2.hpp"
 
 #include <memory>
 
@@ -9,8 +11,15 @@ namespace vulture{
     class Renderer{
     public:
         Renderer(std::weak_ptr<Window>);
+
         virtual void clear() = 0;
         virtual void present() = 0;
         virtual void drawingColor(const Color &) = 0;
+
+        virtual const Rectangle viewPort() const = 0;
+        virtual void viewPort(const Rectangle &) = 0;
+
+        virtual const Vector2 scale() const = 0;
+        virtual void scale(const Vector2 &) = 0;
     };
 }

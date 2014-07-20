98ebc301b7651d130f66b8864c28bdd43df977ef
spartanPAGE <spartanpageagain@gmail.com>
2014-07-20 13:56:41 +0200

    Shh... small letter...

--- include/windows/WindowEventsPoller.hpp
+++ include/windows/WindowEventsPoller.hpp
@@ -8,7 +8,7 @@ namespace vulture{
     class WindowEventsPoller{
     public:
         void registerWindow(std::weak_ptr<Window>);
-        virtual void PollEvents() = 0;
+        virtual void pollEvents() = 0;
     protected:
         typedef std::map<uint32_t, std::weak_ptr<Window>> container_type;
         container_type windows_container;

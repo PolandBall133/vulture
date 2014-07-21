07153e52dfb462e50b5e94a6df6246483ee60844
spartanPAGE <spartanpageagain@gmail.com>
2014-07-22 00:45:20 +0200

    Included "windows/WindowEvents" header; All methods of IEventHandle interface now takes specified const reference to `WindowEvent::*` class

--- include/windows/IWindowEventHandler.hpp
+++ include/windows/IWindowEventHandler.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
 #include <cinttypes>
+#include "windows/WindowEvents.hpp"
 
 namespace vulture{
     class IEventHandler{
-        virtual void onShown(){}
-        virtual void onExposed(){}
-        virtual void onResized(){}
-        virtual void onMinimized(){}
-        virtual void onMaximized(){}
-        virtual void onRestored(){}
-        virtual void onClosed(){}
+        virtual void onShown(const WindowEvent::Shown &){}
+        virtual void onExposed(const WindowEvent::Exposed &){}
+        virtual void onResized(const WindowEvent::Resized &){}
+        virtual void onMinimized(const WindowEvent::Minimized &){}
+        virtual void onMaximized(const WindowEvent::Maxizmied &){}
+        virtual void onRestored(const WindowEvent::Restored &){}
+        virtual void onClosed(const WindowEvent::Closed &){}
     };
 }

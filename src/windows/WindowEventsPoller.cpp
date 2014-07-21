d997baaa2b66c4d1d4fbc6a5d059e11c386d4648
spartanPAGE <spartanpageagain@gmail.com>
2014-07-22 01:10:08 +0200

    Added `WindowEventsPoller::callWindowEvent` definition, which:   - makes an reference to chosen window;   - by switch based on `WindowEvent::Type` calls required event-handling-method (*   note.1: I'll use these types of in-commits comments   note.2: I don't this way of implementing this, but i don't see other ways :C *)

--- src/windows/WindowEventsPoller.cpp
+++ src/windows/WindowEventsPoller.cpp
@@ -6,4 +6,32 @@ namespace vulture{
         uint32_t id = window.lock().get()->ID();
         windows_container[id] = window;
     }
+
+    void
+    WindowEventsPoller::callWindowEvent(const WindowEvent &we, uint32_t id){
+        Window &window = *windows_container[id].lock().get();
+        switch(we.type()){
+        case WindowEvent::Type::Shown:
+            window.onShown(we.event().shown);
+            break;
+        case WindowEvent::Type::Exposed:
+            window.onShown(we.event().exposed);
+            break;
+        case WindowEvent::Type::Moved:
+            window.onShown(we.event().moved);
+            break;
+        case WindowEvent::Type::Resized:
+            window.onShown(we.event().resized);
+            break;
+        case WindowEvent::Type::Minimized:
+            window.onShown(we.event().minimized);
+            break;
+        case WindowEvent::Type::Maximized:
+            window.onShown(we.event().maxizmied);
+            break;
+        case WindowEvent::Type::Restored:
+            window.onShown(we.event().restored);
+            break;
+        }
+    }
 }

9bdb77992541415df1500a5956b30250f296ee0d
spartanPAGE <spartanpageagain@gmail.com>
2014-07-22 00:59:42 +0200

    renamed `IEventHandler` to `IWindowEventHandler`; All methods of `IWindowEventHandler` interface are now public

--- include/windows/IWindowEventHandler.hpp
+++ include/windows/IWindowEventHandler.hpp
@@ -4,12 +4,13 @@
 #include "windows/WindowEvents.hpp"
 
 namespace vulture{
-    class IEventHandler{
+    class IWindowEventHandler{
+    public:
         virtual void onShown(const WindowEvent::Shown &){}
         virtual void onExposed(const WindowEvent::Exposed &){}
         virtual void onResized(const WindowEvent::Resized &){}
         virtual void onMinimized(const WindowEvent::Minimized &){}
-        virtual void onMaximized(const WindowEvent::Maxizmied &){}
+        virtual void onMaximized(const WindowEvent::Maximized &){}
         virtual void onRestored(const WindowEvent::Restored &){}
         virtual void onClosed(const WindowEvent::Closed &){}
     };

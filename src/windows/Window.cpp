37852f2f405420807324ad1caccb693085c8afd4
spartanPAGE <spartanpageagain@gmail.com>
2014-07-23 00:33:27 +0200

    Added defintions. Nothing really interesting

--- src/windows/Window.cpp
+++ src/windows/Window.cpp
@@ -2,4 +2,14 @@
 
 namespace vulture{
     Window::Window(const std::string &title, const Rectangle &rect, const Flags flags){}
+
+    bool
+    Window::closed() const{
+        return _closed;
+    }
+
+    void
+    Window::closed(bool val){
+        _closed = val;
+    }
 }

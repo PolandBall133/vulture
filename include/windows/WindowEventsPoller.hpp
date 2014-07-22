bb58778949f277591e9ef3e1efc14f13d7a4fa81
spartanPAGE <spartanpageagain@gmail.com>
2014-07-23 00:38:39 +0200

    Refractoring. Shh.

--- include/windows/WindowEventsPoller.hpp
+++ include/windows/WindowEventsPoller.hpp
@@ -9,13 +9,12 @@ namespace vulture{
     class WindowEventsPoller{
     public:
         void registerWindow(std::weak_ptr<Window>);
+        void unregisterWindow(uint32_t);
+        bool windowRegistered(uint32_t) const;
         virtual void poll() = 0;
     protected:
         void callWindowEventHandler(const WindowEvent &, uint32_t);
     private:
-        bool isWindowExpired(uint32_t) const;
-        void unregisterWindow(uint32_t);
-        void callSpecifiedWindowEventHandler(const WindowEvent &, Window &);
         typedef std::map<uint32_t, std::weak_ptr<Window>> container_type;
         container_type windows_container;
     };

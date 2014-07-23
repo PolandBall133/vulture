1fe143df258073e7cfe501a2037424450136429e
spartanPAGE <spartanpageagain@gmail.com>
2014-07-23 23:54:57 +0200

    some more namespaces

--- include/graphics/Renderer.hpp
+++ include/graphics/Renderer.hpp
@@ -9,21 +9,23 @@
 #include <memory>
 
 namespace vulture{
-    class Renderer{
-    public:
-        Renderer(std::weak_ptr<windows::Window>);
+    namespace graphics{
+        class Renderer{
+        public:
+            Renderer(std::weak_ptr<windows::Window>);
 
-        virtual void clear() = 0;
-        virtual void present() = 0;
-        virtual void drawingColor(const Color &) = 0;
+            virtual void clear() = 0;
+            virtual void present() = 0;
+            virtual void drawingColor(const Color &) = 0;
 
-        virtual const Rectangle viewPort() const = 0;
-        virtual void viewPort(const Rectangle &) = 0;
+            virtual const Rectangle viewPort() const = 0;
+            virtual void viewPort(const Rectangle &) = 0;
 
-        virtual const Vector2 scale() const = 0;
-        virtual void scale(const Vector2 &) = 0;
+            virtual const Vector2 scale() const = 0;
+            virtual void scale(const Vector2 &) = 0;
 
-        virtual const Size logicalSize() const = 0;
-        virtual void logicalSize(const Size &) = 0;
-    };
+            virtual const Size logicalSize() const = 0;
+            virtual void logicalSize(const Size &) = 0;
+        };
+    }
 }
--- src/graphics/Renderer.cpp
+++ src/graphics/Renderer.cpp
@@ -1,6 +1,8 @@
 #include "graphics/Renderer.hpp"
 
 namespace vulture{
-    using namespace windows;
-    Renderer::Renderer(std::weak_ptr<Window>){}
+    namespace graphics{
+        using namespace windows;
+        Renderer::Renderer(std::weak_ptr<Window>){}
+    }
 }

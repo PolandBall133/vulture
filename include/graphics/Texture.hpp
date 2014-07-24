7eb11cbdda156af523122d502d313957078f41a2
spartanPAGE <spartanpageagain@gmail.com>
2014-07-25 00:30:54 +0200

    Added `graphics/Texture` header with `Texture` class

---
+++ include/graphics/Texture.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Color.hpp"
+#include "Vector2.hpp"
+#include "Size.hpp"
+
+#include <memory>
+
+namespace vulture{
+    namespace graphics{
+        class Texture{
+        };
+    }
+}

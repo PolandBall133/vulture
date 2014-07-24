1442be74eb894db42c5d1bad5eccf4e33d2efb73
spartanPAGE <spartanpageagain@gmail.com>
2014-07-25 00:36:13 +0200

    Added `Teture::load(renderer, name)` abstract method

--- include/graphics/Texture.hpp
+++ include/graphics/Texture.hpp
@@ -4,11 +4,14 @@
 #include "Vector2.hpp"
 #include "Size.hpp"
 
+#include <string>
 #include <memory>
 
 namespace vulture{
     namespace graphics{
         class Texture{
+        public:
+            void load(Renderer &, const std::string &) = 0;
         };
     }
 }

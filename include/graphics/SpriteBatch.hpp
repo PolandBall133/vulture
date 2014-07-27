fee0180953abce4cf724089a641861898ccd4fd3
spartanPAGE <spartanpageagain@gmail.com>
2014-07-27 14:02:42 +0200

    in `graphics::SpriteBatch`:   - added abstract `begin()`   - added abstract `end()`   - added abstract `backgroundColor() -> Color` getter   - added abstract `backgroundColor(Color)` setter

--- include/graphics/SpriteBatch.hpp
+++ include/graphics/SpriteBatch.hpp
@@ -3,6 +3,10 @@
 namespace vulture{
     namespace graphics{
         class SpriteBatch{
+            virtual void begin() = 0;
+            virtual void end() = 0;
+            virtual const Color backgroundColor() const = 0;
+            virtual void backgroundColor(const Color &) const = 0;
         };
     }
 }

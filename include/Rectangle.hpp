9b13de6a5475780c885a585de5559d053f2d1772
spartanPAGE <spartanpageagain@gmail.com>
2014-07-29 00:35:36 +0200

    In `vulture::Rectangle`:   fixed:     types of `width, and height`     constructor types   added:     constructor `(point, size)`     `{position, size}` getters

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 #include <cinttypes>
+#include "Point.hpp"
+#include "Size.hpp"
 
 namespace vulture{
 
     struct Rectangle{
-        int32_t x, y, width, height;
-        Rectangle(int32_t = 0, int32_t = 0, int32_t = 0, int32_t = 0);
+        int32_t x, y;
+        uint32_t width, height;
+
+        Rectangle(int32_t = 0, int32_t = 0, uint32_t = 0, uint32_t = 0);
+        Rectangle(const Point &, const Size &);
+
+        const Point position() const;
+        const Size size() const;
 
         bool isEmpty() const;
         bool equals(const Rectangle &) const;

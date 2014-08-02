9cb67c7113070601585ba488418df2a7b4a48b2d
spartanPAGE <spartanpageagain@gmail.com>
2014-08-02 23:39:37 +0200

    Not in joys! `Manager::storage` is now `map<id, ptr>`; Added `byId` request

--- include/input/devices/Manager.hpp
+++ include/input/devices/Manager.hpp
@@ -2,52 +2,66 @@
 
 #include "input/devices/joy/Joy.hpp"
 #include <memory>
-#include <list>
+#include <map>
 
 namespace vulture{
     namespace input{
         namespace devices{
-            namespace joys{
-                template<class Type>
-                class Manager{
-                    friend class Requestor;
-                    friend class EventsPoller;
-                public:
-                    typedef Type managed_type;
-                    typedef std::shared_ptr<managed_type> pointer_type;
-                    typedef std::list<pointer_type> container_type;
+            template<class Type>
+            class Manager{
+                friend class Requestor;
+                friend class EventsPoller;
+            public:
+                typedef Type managed_type;
+                typedef std::shared_ptr<managed_type> pointer_type;
+                typedef std::map<uint32_t, pointer_type> container_type;
+            private:
+                class Requestor{
                 private:
-                    class Requestor{
-                    private:
-                        Manager &_m;
-                    public:
-                        Requestor(Manager &);
-                        Manager::container_type &all();
-                        const Manager::container_type &all() const;
-                    };
-                    container_type _storage;
-                    std::shared_ptr<Requestor> _requestor;
+                    Manager &_m;
                 public:
-                    Manager();
-                    Requestor &request();
-                    const Requestor &request() const;
+                    Requestor(Manager &);
+                    Manager::container_type &all();
+                    const Manager::container_type &all() const;
+                    Manager::pointer_type byID(uint32_t);
+                    const Manager::pointer_type byID(uint32_t) const;
                 };
+                container_type _storage;
+                std::shared_ptr<Requestor> _requestor;
+            public:
+                Manager();
+                Requestor &request();
+                const Requestor &request() const;
+            };
 
-                template<class T>
-                Manager<T>::Requestor::Requestor(Manager &m):
-                    _m(m){}
+            template<class T>
+            Manager<T>::Requestor::Requestor(Manager &m):
+                _m(m){}
 
-                template<class T>
-                typename Manager<T>::container_type &
-                Manager<T>::Requestor::all(){
-                    return _m._storage;
-                }
+            template<class T>
+            typename Manager<T>::container_type &
+            Manager<T>::Requestor::all(){
+                return _m._storage;
+            }
 
-                template<class T>
-                const typename Manager<T>::container_type &
-                Manager<T>::Requestor::all() const{
-                    return _m._storage;
-                }
+            template<class T>
+            const typename Manager<T>::container_type &
+            Manager<T>::Requestor::all() const{
+                return _m._storage;
+            }
+
+            template<class T>
+            typename Manager<T>::pointer_type
+            Manager<T>::Requestor::byID(uint32_t id){
+                return _m._storage.count(id)?
+                    _m._storage[id] : nullptr;
+            }
+
+            template<class T>
+            const typename Manager<T>::pointer_type
+            Manager<T>::Requestor::byID(uint32_t id) const{
+                return _m._storage.count(id)?
+                    _m._storage[id] : nullptr;
             }
         }
     }

969968e7fe44dd83f6a2bf3ff407e033f52b689f
spartanPAGE <spartanpageagain@gmail.com>
2014-08-02 21:12:14 +0200

    Added filled `input/devices/Manager` header

---
+++ include/input/devices/Manager.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "input/devices/joy/Joy.hpp"
+#include <memory>
+#include <list>
+
+namespace vulture{
+    namespace input{
+        namespace devices{
+            namespace joys{
+                template<class Type>
+                class Manager{
+                    friend class Requestor;
+                    friend class EventsPoller;
+                public:
+                    typedef Type managed_type;
+                    typedef std::shared_ptr<managed_type> pointer_type;
+                    typedef std::list<pointer_type> container_type;
+                private:
+                    class Requestor{
+                    private:
+                        Manager &_m;
+                    public:
+                        Requestor(Manager &);
+                        Manager::container_type &all();
+                        const Manager::container_type &all() const;
+                    };
+                    class EventsPoller{
+                    private:
+                        Manager &_m;
+                    protected:
+                        Manager &manager();
+                    public:
+                        EventsPoller(Manager &);
+                        virtual void poll() = 0;
+                    };
+                    container_type _storage;
+                    std::shared_ptr<Requestor> _requestor;
+                public:
+                    Manager();
+                    Requestor &request();
+                    const Requestor &request() const;
+                };
+
+                template<class T>
+                Manager<T>::Requestor::Requestor(Manager &m):
+                    _m(m){}
+
+                template<class T>
+                typename Manager<T>::container_type &
+                Manager<T>::Requestor::all(){
+                    return _m._storage;
+                }
+
+                template<class T>
+                const typename Manager<T>::container_type &
+                Manager<T>::Requestor::all() const{
+                    return _m._storage;
+                }
+
+                template<class T>
+                Manager<T>::Manager(){
+                    _requestor = std::make_shared<Requestor>(*this);
+                }
+
+                template<class T>
+                Manager<T>::EventsPoller::EventsPoller(Manager &m):
+                    _m(m){}
+            }
+        }
+    }
+}

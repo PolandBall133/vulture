bfd3b0c37c2676facb16fa0f38b582d77d84db48
spartanPAGE <spartanpageagain@gmail.com>
2014-08-02 13:53:30 +0200

    new tests

--- include/util/tests/ManagerTests.hpp
+++ include/util/tests/ManagerTests.hpp
@@ -13,12 +13,14 @@ namespace vulture{
         class ManagerFixture : public CppUnit::TestFixture{
             CPPUNIT_TEST_SUITE(ManagerFixture);
                 CPPUNIT_TEST(factory_creating_test);
+                CPPUNIT_TEST(using_references_test);
             CPPUNIT_TEST_SUITE_END();
         private:
             typedef Manager<int> manager_type;
             manager_type m;
         public:
             void factory_creating_test();
+            void using_references_test();
         };
     }
 }

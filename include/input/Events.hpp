dbd5e3edf818360cf09d79b1f13369619cf15456
spartanPAGE <spartanpageagain@gmail.com>
2014-08-02 13:13:31 +0200

    Filled with `Joy` sutff

--- include/input/Events.hpp
+++ include/input/Events.hpp
@@ -1,9 +1,16 @@
 #pragma once
+#include "input/devices/joy/Joy.hpp"
 
 namespace vulture{
     namespace input{
         namespace devices{
             class Event{
+                enum class Type : uint32_t{
+                    Joy
+                };
+                union Events{
+                    joys::Joy joy;
+                };
             };
         }
     }

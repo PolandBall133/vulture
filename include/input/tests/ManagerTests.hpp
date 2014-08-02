c8fab9599a36bb79a1a0822754474867e7346017
spartanPAGE <spartanpageagain@gmail.com>
2014-08-02 13:17:02 +0200

    Added `ManagerTests` header

---
+++ include/input/tests/ManagerTests.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestSuite.h>
+#include <cppunit/TestCase.h>
+
+
+namespace vulture{
+    namespace test{
+    }
+}

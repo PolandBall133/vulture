b97f264cad44fca99e7b1f35193ae814b2a3345f
spartanPAGE <spartanpageagain@gmail.com>
2014-08-04 16:15:19 +0200

    Included `cintypes`;
    In `vulture::events::WindowEvent`:
      - added public `Type` enum

--- include/events/WindowEvent.hpp
+++ include/events/WindowEvent.hpp
@@ -1,9 +1,12 @@
 #pragma once
+#include <cinttypes>
 
 namespace vulture{
     namespace events{
         class WindowEvent{
         public:
+            enum Type : uint32_t{
+            };
         protected:
         private:
         };

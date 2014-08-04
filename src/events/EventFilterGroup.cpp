036a7d65ad5b1e6c1bb5679276e81348cafb19c7
spartanPAGE <spartanpageagain@gmail.com>
2014-08-05 01:14:34 +0200

    Add some definitions.

---
+++ src/events/EventFilterGroup.cpp
@@ -0,0 +1,20 @@
+#include "events/EventsFilterGroup.hpp"
+
+using namespace std;
+namespace vulture{
+    namespace events{
+        EventsFilterGroup::EventsFilterGroup(
+            const string &name
+        ): _name(name){}
+
+        const string &
+        EventsFilterGroup::name() const{
+            return _name;
+        }
+
+        EventsFilterGroup &
+        EventsFilterGroup::add(shared_ptr<EventsFilter> efptr){
+            _eventsFilters.push_back(efptr);
+        }
+    }
+}

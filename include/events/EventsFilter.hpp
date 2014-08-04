e5c2cff741ce0a2efa9a5db1f4abc9e75d821d1b
spartanPAGE <spartanpageagain@gmail.com>
2014-08-05 01:21:18 +0200

    Included string.
    in `vulture::events::EventsFilter`:
      - added public `constructor(string)`
      - added public `name()` getter
      - added private `_name` field

--- include/events/EventsFilter.hpp
+++ include/events/EventsFilter.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include "events/Events.hpp"
+#include <string>
 
 namespace vulture{
     namespace events{
         class EventsFilter{
         public:
+            EventsFilter(const std::string &);
             virtual void filter(const Events &) = 0;
+            const std::string &name() const;
+        private:
+            const std::string _name;
         };
     }
 }

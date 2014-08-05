047fe92c8d8ed235f7758ec96a4f82f15de90f94
spartanPAGE <spartanpageagain@gmail.com>
2014-08-05 13:05:51 +0200

    In `vulture::events::EventsFilter`:   - abstract `filter(Event)` now returns `bool`

--- include/events/EventsFilter.hpp
+++ include/events/EventsFilter.hpp
@@ -7,7 +7,7 @@ namespace vulture{
         class EventsFilter{
         public:
             EventsFilter(const std::string &);
-            virtual void filter(const Events &) = 0;
+            virtual bool filter(const Events &) = 0;
             const std::string &name() const;
         private:
             const std::string _name;

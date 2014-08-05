b40700baaaec3f74f78e6ab1c5b65be0121bb676
spartanPAGE <spartanpageagain@gmail.com>
2014-08-05 13:15:45 +0200

    In `vulture::events::System`:   - added public `addFilters(shared_ptr<FiltersGroup>)` method   - added protected list<sharded_ptr<FiltersGroup>> _eventsFiltersGroup` field

--- include/events/System.hpp
+++ include/events/System.hpp
@@ -1,14 +1,17 @@
 #pragma once
 #include "events/Events.hpp"
-#include "events/EventsFilter.hpp"
-#include "list"
+#include "events/EventsFilterGroup.hpp"
+#include <list>
+#include <memory>
 
 namespace vulture{
     namespace events{
         class System{
         public:
             virtual void poll() = 0;
+            void addFilters(std::shared_ptr<EventsFilterGroup>);
         protected:
+            std::list<std::shared_ptr>> _eventsFilterGroups;
         private:
         };
     }

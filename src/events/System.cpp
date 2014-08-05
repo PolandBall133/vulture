580963e94e7340815726b47c81cdbe9f5b200994
spartanPAGE <spartanpageagain@gmail.com>
2014-08-05 13:24:45 +0200

    In `vulture::events::System`:   - added protected `handleEvents(events)` method

--- src/events/System.cpp
+++ src/events/System.cpp
@@ -7,5 +7,12 @@ namespace vulture{
         System::addFilters(shared_ptr<EventsFilterGroup> efgptr){
             _eventsFilterGroups.push_back(efgptr);
         }
+
+        void
+        System::handleEvent(const Events &events){
+            for(auto &filterGroupptr : _eventsFilterGroups)
+                if(filterGroupptr->filter(events))
+                    return;
+        }
     }
 }

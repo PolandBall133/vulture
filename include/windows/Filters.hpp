f14a86b0f8a856b7c9c24086d0323cbc670e5099
spartanPAGE <spartanpageagain@gmail.com>
2014-08-14 20:04:46 +0200

    Moved macro + renamed it

--- include/windows/Filters.hpp
+++ include/windows/Filters.hpp
@@ -1,15 +1,7 @@
 #pragma once
 
 #include <cinttypes>
-#include "events/FilterGroup.hpp"
-
-#define VULTURE_WINDOWS_DECLARE_FILTER(name) \
-    class name##Filter: \
-        public events::Filter{ \
-    public: \
-        name##Filter(); \
-        virtual bool filter(const events::Event &) override; \
-    }
+#include "events/FiltersGroup.hpp"
 
 namespace vulture{
     namespace windows{
@@ -21,13 +13,13 @@ namespace vulture{
             virtual bool checkType(const events::Event &) override;
         };
 
-        VULTURE_WINDOWS_DECLARE_FILTER(Shown);
-        VULTURE_WINDOWS_DECLARE_FILTER(Exposed);
-        VULTURE_WINDOWS_DECLARE_FILTER(Moved) ;
-        VULTURE_WINDOWS_DECLARE_FILTER(Resized);
-        VULTURE_WINDOWS_DECLARE_FILTER(Minimized);
-        VULTURE_WINDOWS_DECLARE_FILTER(Maximized);
-        VULTURE_WINDOWS_DECLARE_FILTER(Restored);
-        VULTURE_WINDOWS_DECLARE_FILTER(Closed);
+        VULTURE_DECLARE_FILTER(Shown);
+        VULTURE_DECLARE_FILTER(Exposed);
+        VULTURE_DECLARE_FILTER(Moved) ;
+        VULTURE_DECLARE_FILTER(Resized);
+        VULTURE_DECLARE_FILTER(Minimized);
+        VULTURE_DECLARE_FILTER(Maximized);
+        VULTURE_DECLARE_FILTER(Restored);
+        VULTURE_DECLARE_FILTER(Closed);
     }
 }

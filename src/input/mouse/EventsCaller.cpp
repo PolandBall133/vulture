cdcacfe8cc7ee7080046c352e51fd4a497e3d43e
spartanPAGE <spartanpageagain@gmail.com>
2014-08-15 02:54:54 +0200

    added some filters. and files. and.. ugh

---
+++ src/input/mouse/EventsCaller.cpp
@@ -0,0 +1,35 @@
+#include "input/Mouse/EventsCaller.hpp"
+
+#define VULTURE_INPUT_MOUSE_DECLARE_INJECTED_UPDATER_FILTER(name, lowercasename) \
+    EventsCaller:: name##Filter:: name##Filter( \
+        Updater &updater \
+    ): _updater(updater){} \
+    bool \
+    EventsCaller:: name##Filter::filter(const Event &event){ \
+        if(!mouse:: name##Filter::filter(event)) return false; \
+        _updater(event.events.mouse.events. lowercasename); \
+        return true; \
+    }
+
+using namespace std;
+namespace vulture{
+    using namespace events;
+    namespace input{
+        namespace mouse{
+            VULTURE_INPUT_MOUSE_DECLARE_INJECTED_UPDATER_FILTER(Button, button);
+            VULTURE_INPUT_MOUSE_DECLARE_INJECTED_UPDATER_FILTER(Motion, motion);
+            VULTURE_INPUT_MOUSE_DECLARE_INJECTED_UPDATER_FILTER(Wheel, wheel);
+
+            EventsCaller::EventsCaller(
+                System &system, Updater &updater
+            ){
+                auto filtersGroup = make_shared<FiltersGroup>();
+                (*filtersGroup.get())
+                    .add(make_shared<ButtonFilter>(updater))
+                    .add(make_shared<MotionFilter>(updater))
+                    .add(make_shared<WheelFilter>(updater));
+                system.addFilters(filtersGroup);
+            }
+        }
+    }
+}
